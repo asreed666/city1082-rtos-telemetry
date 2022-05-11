fce944c989dc4c221e572c0c789e20ef0f11fd3f
Andrew <areed@cityplym.ac.uk>
2022-05-11 15:48:06 +0100

    chris initial

--- src/config.h
+++ src/config.h
@@ -2,13 +2,13 @@
  * Edit this file updating your Thing name and Topic definitions
  */
 
-#define THING_NAME "CCP05"  // change this to a unique IOT device name for the MQTT Broker
+#define THING_NAME "CCP06"  // change this to a unique IOT device name for the MQTT Broker
 #define LIGHT_SET_TOPIC "/lightSet"
-#define LIGHT_LEVEL_SET_TOPIC "CCP05/lightSet"
+#define LIGHT_LEVEL_SET_TOPIC "CCP06/lightSet"
 #define LIGHT_LEVEL_TOPIC "/lightLevel"
 #define ANNOUNCE_TOPIC "announce"
 #define TEMP_SET_TOPIC "/tempSet"
-#define TEMPERATURE_SET_TOPIC "CCP05/tempSet"
+#define TEMPERATURE_SET_TOPIC "CCP06/tempSet"
 #define TEMPERATURE_TOPIC "/temp"
 #define HEATER_STATUS "/heaterStatus"
 #define LIGHT_STATUS "/lightStatus"
