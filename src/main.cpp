f9ffc60fa78638c9d707e00820992269ce9bdc2d
asreed666 <areed@cityplym.ac.uk>
2024-03-27 15:47:27 +0000

    timers not working as expected?

--- src/main.cpp
+++ src/main.cpp
@@ -82,7 +82,8 @@ int main(void) {
         timeEchoUs = 0;
         trigger = 1; // start measurements
         trigTime.start();
-        while (trigTime.read_us() < 11) {trigger = 1;}
+        int elapsed = trigTime.read_us();
+        while ((trigTime.read_us() - elapsed) < 11) {trigger = 1;}
         trigger = 0;
         echoTime.start();
         ThisThread::sleep_for(500);
